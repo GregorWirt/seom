9b81b10c86c7707c56c5d0a2a2e59006f1df15be
Gregor Wirtinger <wir20005@spengergasse.at>
2023-02-17 22:00:33 +0100

    Add timesheet.

---
+++ Source/Seom.Application/Dtos/WorkItemDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seom.Application.Dtos
+{
+    public record WorkItemDto(
+        [StringLength(255, MinimumLength = 2)] string Name,
+        DateTime From,
+        DateTime To) : IValidatableObject
+    {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (From < DateTime.UtcNow.AddYears(-1))
+                yield return new ValidationResult("Start date is over 1 year in the past.", new string[] { nameof(From) });
+            if (To > DateTime.UtcNow.AddYears(1))
+                yield return new ValidationResult("End date is over 1 year in the future.", new string[] { nameof(From) });
+            if (From > To)
+                yield return new ValidationResult("Start date is after end date.", new string[] { nameof(From), nameof(To) });
+        }
+    }
+}
---
+++ Source/Seom.Webapp/Pages/Timesheet/Index.cshtml.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Seom.Application.Dtos;
+using Seom.Application.Infrastructure;
+using Seom.Application.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Seom.Webapp.Pages.Timesheet
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SeomContext _db;
+
+        public IndexModel(SeomContext db)
+        {
+            _db = db;
+        }
+
+        public SelectList Projects { get; set; } = default!;
+        public List<WorkItem> WorkItems { get; set; } = new();
+        [FromRoute]
+        public Guid ProjectGuid { get; set; }
+        [BindProperty]
+        public WorkItemDto? NewWorkItem { get; set; }
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPostWorkitem()
+        {
+            if (!ModelState.IsValid) { return Page(); }
+            if (NewWorkItem is null) { return RedirectToPage(); }
+            var project = _db.Projects.FirstOrDefault(p => p.Guid == ProjectGuid);
+            if (project is null) { return RedirectToPage(); }
+            var workitem = new WorkItem(project: project, name: NewWorkItem.Name, from: NewWorkItem.From, to: NewWorkItem.To);
+            _db.WorkItems.Add(workitem);
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (DbUpdateException e)
+            {
+                ModelState.AddModelError("", e.Message);
+                return Page();
+            }
+            return RedirectToPage();
+        }
+
+        public override void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+        {
+            Projects = new SelectList(_db.Projects.OrderBy(p => p.Name), nameof(Project.Guid), nameof(Project.Name), "");
+            //Projects = _db.Projects
+            //    .OrderBy(p => p.Name)
+            //    .Select(p => new SelectListItem(p.Name, p.Guid.ToString()))
+            //    .ToList();
+            WorkItems = _db.WorkItems.Where(w => w.Project.Guid == ProjectGuid)
+                .OrderBy(p => p.From)
+                .ToList();
+        }
+    }
+}
